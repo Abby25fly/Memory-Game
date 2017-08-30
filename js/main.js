9956aeb350a0415e6786817318471a9140d0ed9e
Abigail <abi25.fly@gmail.com>
2017-08-30 10:28:52 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,53 @@
+function imgApper() {
+    var imgPet= document.getElementById("imgbox");
+    if (imgPet.style.visibility === 'hidden') {
+        imgPet.style.visibility = 'visible';
+    } else {
+        imgPet.style.visibility = 'hidden';
+    }
+}
+
+function imgApper2() {
+    var imgPet2= document.getElementById("imgbox2");
+    if (imgPet2.style.visibility === 'hidden') {
+        imgPet2.style.visibility = 'visible';
+    } else {
+        imgPet2.style.visibility = 'hidden';
+    }
+}
+
+function imgApper3() {
+    var imgPet3= document.getElementById("imgbox3");
+    if (imgPet3.style.visibility === 'hidden') {
+        imgPet3.style.visibility = 'visible';
+    } else {
+        imgPet3.style.visibility = 'hidden';
+    }
+}
+
+function imgApper4() {
+    var imgPet4= document.getElementById("imgbox4");
+    if (imgPet4.style.visibility === 'hidden') {
+        imgPet4.style.visibility = 'visible';
+    } else {
+        imgPet4.style.visibility = 'hidden';
+    }
+}
+
+function imgApper5() {
+    var imgPet5= document.getElementById("imgbox5");
+    if (imgPet5.style.visibility === 'hidden') {
+        imgPet5.style.visibility = 'visible';
+    } else {
+        imgPet5.style.visibility = 'hidden';
+    }
+}
+
+function imgApper6() {
+    var imgPet6= document.getElementById("imgbox6");
+    if (imgPet6.style.visibility === 'hidden') {
+        imgPet6.style.visibility = 'visible';
+    } else {
+        imgPet6.style.visibility = 'hidden';
+    }
+}
